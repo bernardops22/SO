00e186d7b16f937b5ebe0fffab43f320a1eccbfc
Bernardo Pereira Henriques Barreiros dos Santos <a92439@iscte.pt>
2020-10-20 13:14:28 +0100

    20/10/2020 v2

--- Parte1/cria_pacientes.sh
+++ Parte1/cria_pacientes.sh
@@ -2,20 +2,23 @@
 
 #Acoes iniciais
 rm -f pacientes.txt
-i=1
+i=0
 
 #Pesquisa e recurso a filtros
-cat /etc/passwd | grep "^a" | grep ":1006" | head -10 | while read line 
+cat /etc/passwd | grep "^a" | while read line 
 do
-  identificacao=$(echo $line | cut -d":" -f1 | sed "s/a//")
-  idnumber="${identificacao//[^[:digit:]]/}"
-  idnumber=$(echo "${#idnumber}")
+  if [ $i -lt 10 ]; then
+    identificacao=$(echo $line | cut -d":" -f1 | sed "s/a//")
+    idnumbers="${identificacao//[^[:digit:]]/}"
+    idnumbers=$(echo "${#idnumbers}") #ID com 5 numeros
+    idlength=$(echo "${#identificacao}") #ID com 5 caracteres
   
-  #Filtro que mantem users do tipo aYYYYY com Y um inteiro
-  if [ $idnumber -eq 5 ]; then
-    nome=$(echo $line | cut -d":" -f5 | sed "s/,,,//")
-    echo $identificacao";"$nome";;;a"$identificacao"@iscte-iul.pt;100" >> pacientes.txt
-    i=$(( $i+1 ))
+    #Filtro que mantem users do tipo aYYYYY com Y um inteiro
+    if [[ $idnumbers -eq 5 ]] && [[ $idlength -eq 5 ]]; then
+      nome=$(echo $line | cut -d":" -f5 | sed "s/,,,//")
+      echo $identificacao";"$nome";;;a"$identificacao"@iscte-iul.pt;100" >> pacientes.txt
+      i=$(( $i+1 ))
+    fi
   fi
 done
 

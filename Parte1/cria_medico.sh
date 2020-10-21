444cfa0c3b71a8b14a7db5c0b8387f5bc3c9b573
Bernardo Santos <bphbs@iscte-iul.pt>
2020-10-21 12:41:40 +0100

    21/10/2020 v1

--- Parte1/cria_medico.sh
+++ Parte1/cria_medico.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 #Lancamento de excecoes
-if [ $(echo "${#1}") -eq 0 ] || [ $(echo "${#2}") -eq 0 ] || [ $(echo "${#3}") -eq 0 ] || [ $(echo "${#4}") -eq 0 ] || [[ $1 =~ [0-9]+$ ]] || [[ !($2 =~ [0-9]+$) ]] || [[ $3 =~ [0-9]+$ ]] || [[ !($4 == *[@]*) ]]; then
+if [ -z "$1" ] || [ -z "$3" ] || [[ $1 =~ [0-9]+$ ]] || [[ !($2 =~ [0-9]+$) ]] || [[ $3 =~ [0-9]+$ ]] || [[ !($4 =~ [@]) ]]; then
   echo -e "\nArgumentos invalidos"
   exit
 fi
 
 if [ -e medicos.txt ]; then
-
   #Filtro do enunciado (obrigatorio)
   ids=$(cat medicos.txt | cut -d";" -f2 | grep -w $2 | wc -l)
-  if [ $ids -eq 1 ]; then 
-    echo -e "\nO medico com o numero de cedula profissional $2 ja esta registado\n"
-    
-    #Resultados
-    echo "-----Lista de medicos registados-----"
-    cat medicos.txt | sort
+  if [ $ids -ne 0 ]; then 
+    echo -e "\nO medico com o numero de cedula profissional $2 ja esta registado"
     exit
   fi
 fi
--- Parte1/cria_pacientes.sh
+++ Parte1/cria_pacientes.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-
 #Acoes iniciais
 rm -f pacientes.txt
 i=0
+NUM_MIN_PAC=10
+SALDO_MIN=100
 
 #Pesquisa e recurso a filtros
 cat /etc/passwd | grep "^a" | while read line 
 do
-  if [ $i -lt 10 ]; then
+  if [ $i -lt $NUM_MIN_PAC ]; then
     identificacao=$(echo $line | cut -d":" -f1 | sed "s/a//")
-    idnumbers=$(awk -F '[0-9]' '{print NF-1}' <<< "$identificacao")
+    idnumbers=$(awk -F '[0-9]' '{print NF-1}' <<< "$identificacao") #ID com 5 numeros
     idlength=$(echo "${#identificacao}") #ID com 5 caracteres
   
     #Filtro que mantem users do tipo aYYYYY com Y um inteiro
     if [[ $idnumbers -eq 5 ]] && [[ $idlength -eq 5 ]]; then
       nome=$(echo $line | cut -d":" -f5 | sed "s/,,,//")
-      echo $identificacao";"$nome";;;a"$identificacao"@iscte-iul.pt;100" >> pacientes.txt
-      i=$(( $i+1 ))
+      echo $identificacao";"$nome";;;a"$identificacao"@iscte-iul.pt;"$SALDO_MIN >> pacientes.txt
+      i=$(($i+1))
     fi
   fi
 done
